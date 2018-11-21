8927729fdbeb2837dc23eaf0e622e23556cf28cc
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-22 06:24:23 +0700

    #10 - Set Time Zone to UTC

--- src/main/java/com/hendisantika/springbootuploadmultiplefiletomysql/SpringbootUploadMultipleFileToMysqlApplication.java
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletomysql/SpringbootUploadMultipleFileToMysqlApplication.java
@@ -2,11 +2,27 @@ package com.hendisantika.springbootuploadmultiplefiletomysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        SpringbootUploadMultipleFileToMysqlApplication.class,
+        Jsr310JpaConverters.class
+})
 public class SpringbootUploadMultipleFileToMysqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootUploadMultipleFileToMysqlApplication.class, args);
     }
+
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+//        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
+    }
+
 }
