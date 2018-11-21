73b3d71df34bf2757ec814ff8f342a06f7961ba3
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-22 06:16:15 +0700

    #3 - Add Date Audit Model

---
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletomysql/model/DateAudit.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springbootuploadmultiplefiletomysql.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.Instant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-upload-multiple-file-to-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/11/18
+ * Time: 06.15
+ */
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+@Data
+public abstract class DateAudit {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+}
