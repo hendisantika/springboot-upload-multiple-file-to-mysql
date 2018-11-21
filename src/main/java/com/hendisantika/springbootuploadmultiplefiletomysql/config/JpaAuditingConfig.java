c29c778c982ae8d1fa7ae543acc88fb97b5f19f0
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-22 06:19:42 +0700

    #7 - Add JPA Auditing Config

---
+++ src/main/java/com/hendisantika/springbootuploadmultiplefiletomysql/config/JpaAuditingConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootuploadmultiplefiletomysql.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-upload-multiple-file-to-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/11/18
+ * Time: 06.19
+ */
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
